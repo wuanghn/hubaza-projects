f8111cde42dbcbca75e91353bdf410477587389f
wuanghn <wuanghn@gmail.com>
2015-06-29 14:38:05 +0700

    2:38

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -1,86 +1,15 @@
 <?php
 
-class PostsController extends \BaseController {
-
-	/**
-	 * Display a listing of the resource.
-	 * GET /posts
-	 *
-	 * @return Response
-	 */
-	public function index()
+class PostsController extends \BaseController 
+{
+	public function getIndex()
 	{
-		//
+		// 
 	}
 
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /posts/create
-	 *
-	 * @return Response
-	 */
-	public function create()
+	public function getTestImage()
 	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /posts
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 * GET /posts/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /posts/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /posts/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /posts/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
+		// 
 	}
 
 }
