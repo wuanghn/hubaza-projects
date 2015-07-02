0ed6118615df5b26437a74f19a1d7e17fc096bda
wuanghn <wuanghn@gmail.com>
2015-07-02 10:40:42 +0700

    commit 10:40

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -57,30 +57,51 @@ class PostsController extends \BaseController
 
 
 
+
 	public function postStore()
 	{
-		$save = nl2br(Input::get('body'));
-
-		$save = str_replace("<br />", "", $save);
-
 		require app_path().'/start/boximage/Box.php';
 		require app_path().'/start/boximage/Color.php';
 
-		$im = imagecreatetruecolor(600, 375);
-		$backgroundColor = imagecolorallocate($im, 51, 153, 255);
-		imagefill($im, 0, 0, $backgroundColor);
+		//dd(Input::all());
+
+		$width = 600;
+
+		if(! Input::hasFile('image'))
+		{
+			// DEFAULT public/uploads/default/default.png
+			$im = imagecreatefromjpeg(public_path('uploads/default/default.jpg'));
+			$height = 600;
+		}else
+		{
+			// have file upload
+		}
+		$save = nl2br(Input::get('body'));
+
+		$save = str_replace("<br />", "", $save)."\n\n\n- Quang Author -";
+
+		// $im = imagecreatetruecolor(600, 375);
+		// $backgroundColor = imagecolorallocate($im, 51, 153, 255);
+		// imagefill($im, 0, 0, $backgroundColor);
 
 		$box = new Box($im);
-		$box->setFontFace(public_path('HelveticaNeue-Medium.ttf')); // http://www.dafont.com/franchise.font
+		$box->setFontFace(public_path('HelveticaNeue-Medium.ttf')); 
 		$box->setFontColor(new Color(255, 255, 255));
-		//$box->setTextShadow(new Color(0, 0, 0, 50), 2, 2);
 		$box->setFontSize(24);
-		$box->setBox(20, 20, 600, 375);
+		$box->setBox(20, 20, $width, $height);
 		$box->setTextAlign('center', 'center');
 		$box->draw($save);
 
 		header("Content-type: image/png");
 		imagepng($im);
+	}
+
+
+
+
+	public function getResize()
+	{
+		Image::make(public_path('uploads/default/default.png'))->resize(600, 600)->save(public_path('uploads/default/default.jpg'));
 	}
 
 }
