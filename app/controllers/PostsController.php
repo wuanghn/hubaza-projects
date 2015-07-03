761c7ae672e4916e93182d6ec2a1b15cefd062a8
wuanghn <wuanghn@gmail.com>
2015-07-03 11:43:47 +0700

    commit

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -35,24 +35,23 @@ class PostsController extends \BaseController
 
 	public function getTestImage()
 	{
-		require app_path().'/start/boximage/Box.php';
-		require app_path().'/start/boximage/Color.php';
+		// Create a 55x30 image
+		$im = imagecreatetruecolor(55, 30);
+		$red = imagecolorallocate($im, 255, 0, 0);
+		$black = imagecolorallocate($im, 0, 0, 0);
 
-		$im = imagecreatetruecolor(600, 375);
-		$backgroundColor = imagecolorallocate($im, 0, 18, 64);
-		imagefill($im, 0, 0, $backgroundColor);
+		// Make the background transparent
+		imagecolortransparent($im, $black);
 
-		$box = new Box($im);
-		$box->setFontFace(public_path('HelveticaNeue-Medium.ttf')); // http://www.dafont.com/franchise.font
-		$box->setFontColor(new Color(255, 75, 140));
-		$box->setTextShadow(new Color(0, 0, 0, 50), 2, 2);
-		$box->setFontSize(24);
-		$box->setBox(20, 20, 460, 460);
-		$box->setTextAlign('center', 'center');
-		$box->draw("Đa tình tự cổ không như hận \n Câu này của ai vậy trời \n");
+		// Draw a red rectangle
+		imagefilledrectangle($im, 4, 4, 50, 25, $red);
 
-		header("Content-type: image/png");
+		// Output and free memory
+		header('Content-type: image/png');
 		imagepng($im);
+		imagedestroy($im);
+		// imagedestroy($image);
+		// imagedestroy($watermark);
 	}
 
 
@@ -66,7 +65,13 @@ class PostsController extends \BaseController
 		//dd(Input::all());
 
 		$width = 600;
+		$radomString = substr(number_format(time() * mt_rand(),0,'',''),0,8);
 
+		if(Input::get('author') == "")
+			$author = "";
+		else
+			$author = "\n\n\n-".Input::get('author')."-";
+		
 		if(! Input::hasFile('image'))
 		{
 			// DEFAULT public/uploads/default/default.png
@@ -74,26 +79,49 @@ class PostsController extends \BaseController
 			$height = 600;
 		}else
 		{
-			// have file upload
+			
+			$height = Input::get('height');
+
+			$filename = public_path("uploads/default/$radomString.jpg");
+
+			Image::make(Input::file('image')->getRealPath())->resize($width,$height)->colorize(-20, -20, -20)->save($filename);
+
+			$im = imagecreatefromjpeg($filename);
+
 		}
 		$save = nl2br(Input::get('body'));
 
-		$save = str_replace("<br />", "", $save)."\n\n\n- Quang Author -";
+		$save = str_replace("<br />", "", $save).$author;
 
-		// $im = imagecreatetruecolor(600, 375);
-		// $backgroundColor = imagecolorallocate($im, 51, 153, 255);
-		// imagefill($im, 0, 0, $backgroundColor);
+
+		// add watermark watermar.png
+		$stamp = imagecreatefrompng(public_path("watermark.png"));
+
+		// Set the margins for the stamp and get the height/width of the stamp image
+		$marge_right = 10;
+		$marge_bottom = 10;
+		$sx = imagesx($stamp);
+		$sy = imagesy($stamp);
+
+		// Copy the stamp image onto our photo using the margin offsets and the photo 
+		// width to calculate positioning of the stamp. 
+		imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
+
+
 
 		$box = new Box($im);
 		$box->setFontFace(public_path('HelveticaNeue-Medium.ttf')); 
 		$box->setFontColor(new Color(255, 255, 255));
-		$box->setFontSize(24);
-		$box->setBox(20, 20, $width, $height);
+		$box->setFontSize(32);
+		$box->setBox(0,0, $width, $height);
 		$box->setTextAlign('center', 'center');
 		$box->draw($save);
 
-		header("Content-type: image/png");
-		imagepng($im);
+		header('Content-Type: image/jpeg');
+		imagejpeg($im); 
+
+		
+		imagejpeg($im, public_path("uploads/store/$radomString.jpg")); 
 	}
 
 
