8066b006300b7043304b8e8372fd8018b41d5ed7
wuanghn <wuanghn@gmail.com>
2015-07-06 15:31:33 +0700

    OAuth wrapper for Laravel 4

--- app/config/app.php
+++ app/config/app.php
@@ -123,6 +123,7 @@ return array(
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
 		'Intervention\Image\ImageServiceProvider',
+		'Artdarek\OAuth\OAuthServiceProvider',
 
 	),
 
@@ -191,6 +192,7 @@ return array(
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
 		'Image' => 'Intervention\Image\Facades\Image',
+		'OAuth' => 'Artdarek\OAuth\Facade\OAuth',
 
 	),
 
