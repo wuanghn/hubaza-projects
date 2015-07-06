d0e949635153d0b420cadee77f4c7eda0dcfd777
wuanghn <wuanghn@gmail.com>
2015-07-06 16:02:25 +0700

    commit master layout update $post

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -10,16 +10,16 @@
     <meta name="author" content="">
 
     <!-- fb -->
-    @if(! $post)
-        <meta property="og:url" content="{{url()}}">
-        <meta property="og:image" itemprop="thumbnailUrl" content="{{asset('/public/assets/img/image.png')}}">
-        <meta property="og:title" itemprop="headline" content="IZquote · The coolest quote generator">
-        <meta property="og:description" content="Create your own quote at: izquote.com">
-    @else
+    @if(isset($post))
         <meta property="og:url" content="{{url($post->slug)}}">
         <meta property="og:image" itemprop="thumbnailUrl" content="{{asset('/public')}}/{{$post->image}}">
         <meta property="og:title" itemprop="headline" content="{{$post->title}}">
         <meta property="og:description" content="{{$post->quote}}">
+    @else
+        <meta property="og:url" content="{{url()}}">
+        <meta property="og:image" itemprop="thumbnailUrl" content="{{asset('/public/assets/img/image.png')}}">
+        <meta property="og:title" itemprop="headline" content="IZquote · The coolest quote generator">
+        <meta property="og:description" content="Create your own quote at: izquote.com">
     @endif
 
 
