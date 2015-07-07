48b449d1ebf591514512d4b007abdb12e330409c
Duc Anh Pham <gahoamone@gmail.com>
2015-07-07 14:57:22 +0700

    update share FB

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -25,12 +25,14 @@ $(document).ready(function(){
         } ).done(function( data ) {
             data2 = JSON.parse(data);
 
+            url = "http://"+window.location.host;
+
             for(i=0; i< data2['contents'].length; i++){
 
                var html ='';
                html = '<div class="col-md-6 div_content_center" skip="'+data2['skip']+'">';
                html +=' <h3>';
-               html +=' <a href="post/'+data2['contents'][i]['slug']+'">'+data2['contents'][i]['title']+'</a>';
+               html +=' <a href="'+url+'/posts/'+data2['contents'][i]['slug']+'">'+data2['contents'][i]['title']+'</a>';
                html +=' </h3>';
                html +=' <p class="lead">';
                html +=' by <a href="index.php">'+data2['contents'][i]['fullname']+'</a>';
@@ -39,7 +41,8 @@ $(document).ready(function(){
                html +='    <hr>';
                html +=' <img class="img-responsive" src="'+data2['contents'][i]['image']+'" alt="">';
                html +='<div class="da_fb_like">';
-               html +=' <a class="btn btn-social btn-facebook">';
+               html +=' <a class="btn btn-social btn-facebook" href="https://www.facebook.com/dialog/feed?app_id=828149473934812&display=popup&caption= '+data2['contents'][i]['title']+'&link='+url+'/posts/'+data2['contents'][i]['slug']+'&redirect_uri='+url+'/post/home">';
+
                html +=' <i class="fa fa-facebook"></i>Facebook</a>';
 
                html +=' <a class="btn btn-social btn-twitter">';
@@ -67,7 +70,7 @@ $(document).ready(function(){
 
 
 
-}
+
 
 
 
