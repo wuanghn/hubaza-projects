9d45cdcb61befbcc06ad8a8e2416c7a6ced0c3a2
Duc Anh Pham <gahoamone@gmail.com>
2015-07-10 15:17:07 +0700

    update no GA

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -63,8 +63,8 @@
         <script src="{{url()}}/public/layouts/default/https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
         <script src="//connect.facebook.net/en_US/all.js"></script>
-
-        <script>
+<!-- 
+     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -74,6 +74,7 @@
       ga('send', 'pageview');
 
       </script>
+       -->
 
   </head>
 
