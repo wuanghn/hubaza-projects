25fdfe4c5cc84d9309a76f1765acbb1e0ab1a625
Duc Anh Pham <gahoamone@gmail.com>
2015-07-14 10:29:25 +0700

    update 2

---
+++ app/views/collections/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.master')
+@section('title',Session::get('info_user')->fullname."'s collection")
+
+
+@section('content')
+<!-- Page Header -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">My Collections
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Projects Row -->
+        <div class="row">
+            @if(count($collection) ==0)
+            <p>Collection is empty !</p>
+
+            @else
+
+            @foreach($collection as $key => $val)
+            <div class="col-md-4 portfolio-item">
+                <div class="caption">
+                    <a onclick=" return confirm('Are you sure you want to delete?')" href="{{url('collect/delete-collection').'/'.$val->id}}" class=""><i class="fa fa-trash-o"></i> Delete</a>
+           
+                </div>
+                <a href="{{url('collect/detail').'/'.$val->id}}">
+                    <img class="img-responsive" src="{{$val->image}}" alt="">
+                </a>
+
+                <h4>
+                    <a href="{{url('collect/detail').'/'.$val->id}}">@if(strlen($val->name) >=70){{substr($val->name,0,70).'...'}}
+                        @else {{$val->name}} @endif
+                    </a>
+                </h4>
+            </div>
+
+            @endforeach
+            @endif
+        </div>
+        <!-- /.row -->
+
+        
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2015</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+@section('script')
+<script type="text/javascript" src="{{asset('public/assets/js/jquery.resizecrop-1.0.3.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('public/assets/js/resize_image.js')}}"></script>
+
+@stop
+
+@stop
+
+
+ 
+
+
+
