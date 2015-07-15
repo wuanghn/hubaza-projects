295d995597149865e037980ded2f72d1422bd45c
Duc Anh Pham <gahoamone@gmail.com>
2015-07-15 11:32:04 +0700

    update like FB

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -100,7 +100,7 @@
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.3&appId=642172092538987";
+      js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.3&appId=828149473934812";
       fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));</script>
 
